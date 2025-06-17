0f11ac0470f60a18c8846c5cc8389b2eb3fbf095
Gabriel Achille <gabriel.achille13@gmail.com>
2025-06-16 22:19:31 -0400

    made changes

--- my-react-vite/src/App.jsx
+++ my-react-vite/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+
+// function displayCatFact(){
+//   const[]
+// }
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,11 +14,12 @@ function App() {
   return (
     <>
       <div>
-        {/* <a 
-        href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a> */}
-        <a href="">🤓</a>
+        </a>
+        <a href="" id="nerdEmoji">
+          🤓
+        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
